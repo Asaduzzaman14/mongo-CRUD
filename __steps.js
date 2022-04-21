b9b2ed392ed571416db293bb6d36c5b13d1611ef
Md Asaduzaman <asadasad9282@gmail.com>
2022-04-22 00:32:52 +0600

    mongodb CRUD

---
+++ __steps.js
@@ -0,0 +1,47 @@
+/**
+ * 1 create a node server with 5 steps
+ * 1.1 create folder
+ * 1.2 npm init
+ * 1.3 npm i express cors mongodb
+ * 1.4 secripts-dev: nodemon index.js
+ * 1.5 create index.js
+ * use 5 steps to creata a node project
+ * 
+ * -----------------
+ * Create Atlas account
+ * ----------------
+ * 1 sign up. google access
+ * 2.create cluster
+ * 3. Create user dbUser1 and password
+ * 4. create Access --> ip addressL allow all
+ * 5. Network Access -> ip address
+ * 
+ * ------------------
+ * CRUD opration
+ * -----------------
+ * 1. node mongodb CRUD -> Fundmantels
+ * 2. Create async run fuction
+ * 
+ * 
+ * -------------------------------------
+ * Intrigate sending data form client to server
+ * ------------------------------------
+ * 
+ * 1. Client side : create from
+ * 2. on submit get form data and create user object
+ * 3. on server: create user post method to reciver data on the backend
+ * 4. on Client side : set fetch with post method to recive data on the backend
+ * 5. make sure you return form the POST API
+ * -------------------------------
+ * LOAD DATA TO THE CLIENT SITE
+ * 1. CREAT get api 
+ * 2.create a  query object
+ * 3. collection.find(query)
+ * 4 cursor.toArray()
+ * 5. return the result
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
